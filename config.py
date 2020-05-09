59563f4f15415f172ea0e213f7349ecff0d43526
Treshank <treshank.prasad@gmail.com>
2020-05-09 11:04:52 +0530

    made public and few changes

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ class Config(object):
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'root'
     MYSQL_DB = 'MyDB'
+    host = '0.0.0.0'
 
     SECRET_KEY = os.environ.get('SECReT_KEY') or 'idk_what_this_is'
